3d445c10b2916c9ad83fdcfc60e5681cd5e64a56
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-15 20:11:19 -0500

    Paso 22) Agregando estilos a archivo App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -36,24 +36,70 @@
 </template>
 
 <style>
+/* Creando estilo de Nav Bar */
+nav {
+    justify-content: center;
+    align-items: center;
+    padding-top: 10px;
+    background-color: var(--color-principal);
+    height: 60px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    box-shadow: 0 0 5px #000;
+}
+nav > ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 35px;
+    margin-right: 35px;
+}
+.active {
+    color: var(--color-quinto);
+    background: var(--color-secundario);
+}
+nav > ul > li > router-link > a:active {
+    color: var(--color-quaternario);
+}
+nav a {
+  text-decoration: none;
+  color: white;
+}
+.a-item {
+    text-decoration: none;
+    padding: 15px;
+    color: var(--color-sexto);
+    font-size: 18px;
+    border-radius: 10px;;
+    line-height: 30px;
+}
+.a-item:hover {
+    color: var(--color-quinto);
+    background: var(--color-secundario);
+}
+.navIcon {
+    display: none;
+}
+/* Paleta de colores */
+* {
+    --color-principal: rgb(1, 104, 112);
+    --color-secundario: rgb(3, 83, 120);
+    --color-terciario: rgb(109, 163, 199);
+    --color-quaternario: rgb(178, 192, 203);
+    --color-quinto: rgb(9, 202, 199);
+    --color-sexto: rgb(255, 255, 255);
+    --color-secciones: rgba(178, 192, 203, 0.342);
+    --input: rgba(7, 66, 61, 0.507);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin: 0;
 }
 </style>
