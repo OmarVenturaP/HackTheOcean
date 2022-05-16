8c19fd6380fd67b75608fe72bafe29a3a735840e
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-16 18:01:57 -0500

    54 Se corrigio la vista del mapa

--- client/src/components/MapComponent.vue
+++ client/src/components/MapComponent.vue
@@ -1,32 +1,63 @@
 <template>
   <div class="container">
 
-    <div class="row">
+    <main>
+      <h1 class="h1">Reporte</h1>
+      <div class="reportform">
+        <form action="Enviar" class="formReport" id="form-report" @submit.prevent="postReport">
 
-      <div class="col-md-8">
+          <section class="formReport">
+            <label for="name" name="name" id="name" class="name">name:</label>
+            <input type="text" name="name" id="name" class="name" placeholder="Nombre" v-model="form.name">
+          </section>
+
+          <section class="formReport">
+            <label for="name" name="name" id="name" class="name">lastname:</label>
+            <input type="text" name="name" id="name" class="name" placeholder="Apellidos" v-model="form.lastname">
+          </section>
+
+          <section class="formReport">
+            <label for="direccion" name="direccion" id="direccion" class="direccion">Dirección: </label>
+            <input id="pac-input" placeholder="Ingresa la localización" type="text" class="pac-target-input" autocomplete="off" v-model="location.adress"><div class="location-by-gps-btn"></div>
+          </section>
+
+
+          <section class="formReport">
+            <label for="contacto" name="contacto" id="contacto" class="contacto">Teléfono de Contacto: </label>
+            <input type="number" name="contacto" id="contacto" class="contacto" placeholder="Teléfono de Contacto">
+          </section>
+
+            <section class="formReport">
+            <label  abel for="correo" name="correo" id="correo" class="correo">Correo Electrónico: </label>
+            <input type="email" name="correo" id="correo" class="correo" placeholder="Correo Electrónico">
+            <button type="submit" class="btn">Enviar</button>
+          </section>
+
+
+        </form>
+
+      </div>
+    </main>
+
+
+
+    <div class="">
+
+      <div class="#map">
         <div class="map_holder">
           <place-search
             v-bind:ready="ready"
-            placeholder="Enter a location"
+            placeholder="Or search here a location"
             loading="Map is loading"
             v-bind:gps_timeout="7000"
             v-bind:fallbackProcedure="fallbackProcedure"
             v-bind:zoom="zoom"
             v-bind:geolocation="geolocation"
             v-bind:address="address"
-            v-bind:manually="manually"
             @changed="getMapData"
           >
           </place-search>
         </div>
-      </div>
-
-      <div class="results">
-        <p>Dirección</p>
-        <p>
-          {{place.address_description}}
-        </p>
-
       </div>
 
     </div>
@@ -35,13 +66,15 @@
 
 <script>
 /* eslint-disable */
+import axios from 'axios';
+
 export default {
-  name: "App",
+  name: "MapComponent",
   data() {
     return {
       ready: true,
       fallbackProcedure: "gps",
-      zoom: 17, //
+      zoom: 17,
       geolocation: {
         lat: 31.73858,
         lng: -35.98628,
@@ -63,6 +96,21 @@ export default {
       },
       place: {},
       form_data: {},
+      
+      location: {
+        lat: '',
+        lng: '',
+        adress: ''
+      },
+
+      form: {
+        name: '',
+        lastname: '',
+        latitude: '',
+        longitude: '',
+        status: 'Open'
+      }
+      
     };
   },
   methods: {
@@ -74,6 +122,20 @@ export default {
       this.fallbackProcedure = "manually";
       this.ready = true;
     },
+
+          // Post request to create a Report
+    postReport() {
+
+      this.form.latitude = this.manually.lat;
+      this.form.longitude = this.manually.lng;
+      console.log(this.form);
+      document.getElementById("form-report").reset();
+      alert("Su reporte se envio de manera satisfactoria")
+
+      axios.post('localhost:3000/reports', this.form)
+        .then( response => console.log(response))
+        .catch( error => console.log(error))
+    }
   },
   created() {},
 };
@@ -81,26 +143,19 @@ export default {
 
 <style>
 .map_holder {
-  position: relative;
-  top: 50px;
-  width: 500px;
-  height: 300px;
-}
-.results {
-  position: relative;
-  border-radius: 5px;
-  border: 2px solid;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  top: -300px;
-  width: 500px;
-  height: 50px;
-  background-color: rgba(13, 26, 38, 0.807);
-  color: white;
+  width: 900px;
+  height: 400px;
+  margin-top: 0px;
+  margin-bottom: 50px;
+  border-radius: 6px;
 }
 .btn {
-  width: 200px;
+  width: 100px;
+  margin: 20px 0;
+  box-shadow: 1px 1px 1px white;
+}
+
+#map {
+  border-radius: 5px;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue';
+import VueGeolocation from 'vue3-geolocation';
+import GMaps from 'vuejs3-google-maps';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(VueGeolocation)
+  .use(GMaps, {
+    load: {
+      apiKey: process.env.VUE_APP_MYKEY,
+      libraries: ['places'],
+    },
+  })
+  .mount('#app');
